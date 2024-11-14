19d65ffac59475109bbfc3371ff53a2b10252787
gbgo <gbgo@cin.ufpe.br>
2024-11-14 11:36:10 -0300

    refactor: atualizada logo da navbar

--- landing-page/src/components/NavBar/Logo.jsx
+++ landing-page/src/components/NavBar/Logo.jsx
@@ -1,8 +1,9 @@
+import logo from "../../assets/LOGO.svg"
+
 export default function Logo() {
   return (
-    <div className="flex flex-col justify-center items-center ">
-      <h1 className="font-poppins font-semibold text-logo-blue text-[50px] ">CREATIC</h1>
-      <p className="font-poppins font-thin text-[14px] -mt-2 tracking-widest text-white">CREATIVE AGENCY</p>
+    <div className="flex flex-col justify-center items-center cursor-pointer  ">
+      <img src={logo} alt="" />
     </div>
       
   );
