86ddf2055021db11e9d8b9e1060c0c5b6ca308a5
gbgo <gbgo@cin.ufpe.br>
2024-11-27 19:04:25 -0300

    refactor: atualizando estilização do texto

--- landing-page/src/components/common/Button.jsx
+++ landing-page/src/components/common/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ title }) {
   return (
-    <button className="bg-black text-white hover:bg-gray-900 text-gray-800 font-poppins py-2 px-4 rounded shadow">
+    <button className="bg-black text-white hover:bg-gray-900 text-sm text-gray-800 font-poppins py-1 px-2 rounded shadow">
       {title}
     </button>
   );
