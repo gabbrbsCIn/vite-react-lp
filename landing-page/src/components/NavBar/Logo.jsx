c7c0e62a127c9cb98e6eefc3dc18e1b3854ca010
gbgo <gbgo@cin.ufpe.br>
2024-11-27 19:04:48 -0300

    refactor: atualizado estilização do tamanho da imagem

--- landing-page/src/components/NavBar/Logo.jsx
+++ landing-page/src/components/NavBar/Logo.jsx
@@ -2,8 +2,8 @@ import logo from "../../assets/LOGO.svg"
 
 export default function Logo() {
   return (
-    <div className="flex flex-col justify-center items-center cursor-pointer  ">
-      <img src={logo} alt="" />
+    <div className="flex flex-col justify-center items-center cursor-pointer max-sm:size-32">
+      <img src={logo} width={180} height={200} alt="" />
     </div>
       
   );
