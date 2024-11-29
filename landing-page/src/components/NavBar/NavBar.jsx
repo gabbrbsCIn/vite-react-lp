b3a9a2a1fef37bf6e7c09ab66c5e605165a2a66f
gbgo <gbgo@cin.ufpe.br>
2024-11-29 19:51:30 -0300

    feat: add navbar para dispositivos mobile

--- landing-page/src/components/NavBar/NavBar.jsx
+++ landing-page/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import Logo from "./Logo";
 import NavButtons from "./NavButtons";
-import { Menu } from 'lucide-react';
+import { Menu, Pointer } from "lucide-react";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleNavBar = () => {
     setIsOpen(!isOpen);
-  }
+  };
   return (
-    <nav className="flex justify-between items-center bg-black h-[120px] px-32 max-lg:px-8">
-      <Logo />
-      <div className="max-lg:hidden">
-        <NavButtons />
-      </div>
-      <div className="lg:hidden">
-        <Menu color="#fff"/>
-      </div>
-    </nav>
-  )
+    <>
+      <nav className="flex justify-between items-center bg-black h-[120px] px-32 max-lg:px-8">
+        <Logo />
+        <div className="max-lg:hidden">
+          <NavButtons />
+        </div>
+        <div className="lg:hidden cursor-pointer">
+          <Menu onClick={toggleNavBar} color="#fff" />
+        </div>
+      </nav>
+      {isOpen && (
+        <nav className="flex items-center flex-col py-6 flex-wrap bg-black">
+          <NavButtons type="col" />
+        </nav>
+      )}
+    </>
+  );
 }
