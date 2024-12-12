67088285831ca53964bbd921d2f36ba36cdbfe2c
gbgo <gbgo@cin.ufpe.br>
2024-12-12 16:38:53 -0300

    feat: add useEffect hook para fechar menu

--- landing-page/src/components/NavBar/NavBar.jsx
+++ landing-page/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
 import NavButtons from "./NavButtons";
 import { Menu } from "lucide-react";
@@ -8,11 +8,25 @@ export default function NavBar() {
   const toggleNavBar = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsOpen(false); 
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
   return (
     <>
       <nav className="flex justify-between items-center bg-black h-[120px] px-32 max-lg:px-8">
         <Logo />
-        <div className="max-lg:hidden">
+        <div className={`max-lg:hidden`} >
           <NavButtons />
         </div>
         <Menu
@@ -21,12 +35,12 @@ export default function NavBar() {
           color="#fff"
         />
       </nav>
-
-      <div className={`overflow-hidden transition-[max-height] duration-500 bg-black ${!isOpen ? 'max-h-96' : 'max-h-0'}`}>
-        <nav className="flex flex-col items-center py-6">
-          <NavButtons type="col"/>
+      <div className={`bg-black overflow-hidden transition-all duration-500  ${isOpen ? 'max-h-56' : 'max-h-0'}`}>
+        <nav className={`flex flex-col transition ${isOpen ? 'animate-fade-down' : ''} items-center py-6`}>
+          <NavButtons type="col" />
         </nav>
       </div>
+
     </>
   );
 }
