b53c4530fee2974fbcfbf89c7c2834d213c5f1ab
Gabriel Barbosa <gbgo@cin.ufpe.br>
2025-01-10 17:32:50 -0300

    feat: add dynamic spacing to NavButtons based on layout type

--- landing-page/src/components/NavBar/NavButtons.jsx
+++ landing-page/src/components/NavBar/NavButtons.jsx
@@ -1,13 +1,13 @@
 import Button from "../common/Button";
 
 export default function NavButtons({ type = "row" }) {
+  const space = type === "col" ? "space-y-3" : "";
+
   return (
-    <nav className={`flex flex-${type}`}>
+    <nav className={`flex flex-${type} ${space}`}>
       <Button title="Home" />
       <Button title="About us" />
       <Button title="Services" />
-      <Button title="Portfolio" />
-      <Button title="Pages" />
       <Button title="Contact us" />
     </nav>
   );
