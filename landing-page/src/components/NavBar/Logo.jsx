5f3e70ffc1f38c7f39d4ca932ec6a0c135933475
Gabriel Barbosa <gbgo@cin.ufpe.br>
2025-01-10 17:33:41 -0300

    refactor: adjust Logo component image size for better layout consistency

--- landing-page/src/components/NavBar/Logo.jsx
+++ landing-page/src/components/NavBar/Logo.jsx
@@ -3,8 +3,8 @@ import logo from "../../assets/LOGO.svg"
 export default function Logo() {
   return (
     <div className="flex flex-col justify-center items-center cursor-pointer max-sm:size-32">
-      <img src={logo} width={180} height={200} alt="" />
+      <img src={logo} width={120} height={200} alt="" />
     </div>
-      
+
   );
 }
