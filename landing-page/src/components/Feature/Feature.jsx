4c22d73d4c12c89e84bf8f39736de3c7af7be489
Gabriel Barbosa <gbgo@cin.ufpe.br>
2025-01-16 13:34:14 -0300

    feat: enhance Feature component with dynamic image selection and improved styling

--- landing-page/src/components/Feature/Feature.jsx
+++ landing-page/src/components/Feature/Feature.jsx
@@ -1,12 +1,12 @@
 import img1 from '../../assets/fabricação.png';
 import img2 from '../../assets/montagem.png';
 import img3 from '../../assets/caldeiraria.jpg';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Feature() {
     const [currentImage, setCurrentImage] = useState(img1);
     const [animate, setAnimate] = useState(false);
-
+    
     const changeImage = (img) => {
         setCurrentImage(img);
         setAnimate("animate-fade animate-ease-out");
@@ -16,7 +16,7 @@ export default function Feature() {
     return (
         <section className="flex flex-col justify-center items-center bg-gray-100 mt-64 py-12 animate-fade-down">
 
-            <h1 className="font-poppins text-blue-900 font-medium text-center text-2xl md:text-3xl lg:text-4xl">
+            <h1 className="font-poppins bg-gradient-to-r from-blue-600 to-blue-800 text-transparent bg-clip-text font-medium text-center text-2xl md:text-3xl lg:text-4xl">
                 Provendo as Melhores Soluções Industriais Sob Medida
             </h1>
             <p className="font-poppins text-gray-500 text-center mt-4 px-4 md:px-12 lg:px-24">
@@ -27,37 +27,37 @@ export default function Feature() {
 
                 <div className="flex flex-col space-y-6 lg:space-y-10 w-full lg:w-1/2">
                     <div
-                        className="flex flex-col items-center cursor-pointer justify-center p-6 bg-blue-900 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out"
+                        className={`flex flex-col items-center transition duration-300 cursor-pointer ${currentImage === img1 ? "bg-blue-900 text-white" : "bg-white text-blue-900"} justify-center p-6 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out`}
                         onClick={() => changeImage(img1)}
                     >
-                        <h1 className="font-poppins text-white font-medium text-lg md:text-xl mt-4">
+                        <h1 className={`font-poppins font-medium text-lg md:text-xl mt-4`}>
                             Manutenção Mecânica e Caldeiraria
                         </h1>
-                        <p className="font-poppins text-sm md:text-base text-gray-300 text-center mt-4">
+                        <p className={`font-poppins ${currentImage === img1 ? "text-gray-300" : "text-blue-800"} text-sm md:text-base text-center mt-4`}>
                             Realizamos soldagens TIG, MIG/MAG e eletrodo revestido, manutenção de caldeiras, bombas centrífugas e fabricação de tubulações em inox e aço carbono. Também oferecemos serviços de usinagem, oxicorte e calandragem, garantindo precisão e alta performance.
                         </p>
                     </div>
 
                     <div
-                        className="flex flex-col items-center cursor-pointer justify-center p-6 bg-blue-900 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out"
+                        className={`flex flex-col items-center transition duration-300 cursor-pointer ${currentImage === img2 ? "bg-blue-900 text-white" : "bg-white text-blue-900"} justify-center p-6 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out`}
                         onClick={() => changeImage(img2)}
                     >
-                        <h1 className="font-poppins text-white font-medium text-lg md:text-xl mt-4">
+                        <h1 className={`font-poppins font-medium text-lg md:text-xl mt-4`}>
                             Fabricação e Montagem de Estruturas
                         </h1>
-                        <p className="font-poppins text-sm md:text-base text-gray-300 text-center mt-4">
+                        <p className={`font-poppins ${currentImage === img2 ? "text-gray-300" : "text-blue-800"} text-sm md:text-base text-center mt-4`}>
                             Desenvolvemos e montamos plataformas, escadas, galpões metálicos, moegas, pipe racks e transportadores de correia. Nossas soluções são robustas e personalizadas para atender às necessidades de cada cliente.
                         </p>
                     </div>
 
                     <div
-                        className="flex flex-col items-center cursor-pointer justify-center p-6 bg-blue-900 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out"
+                        className={`flex flex-col items-center transition duration-300 cursor-pointer ${currentImage === img3 ? "bg-blue-900 text-white" : "bg-white text-blue-900"} justify-center p-6 rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-200 ease-out`}
                         onClick={() => changeImage(img3)}
                     >
-                        <h1 className="font-poppins text-white font-medium text-lg md:text-xl mt-4">
+                        <h1 className={`font-poppins font-medium text-lg md:text-xl mt-4`}>
                             Instalações Industriais Personalizadas
                         </h1>
-                        <p className="font-poppins text-sm md:text-base text-gray-300 text-center mt-4">
+                        <p className={`font-poppins ${currentImage === img3 ? "text-gray-300" : "text-blue-800"} text-sm md:text-base text-center mt-4`}>
                             Executamos a montagem de filtros prensa, bombas de vácuo e equipamentos diversos, além de realizar mudanças de linhas de produção. Garantimos agilidade e eficiência em todas as etapas.
                         </p>
                     </div>
